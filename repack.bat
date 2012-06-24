3ef2c5e374937b0542e81f924f17ac2dfcbccb3d
mrk <mrk@localhost>
2012-06-24 02:03:18 +0200

    0.0.53

---
+++ repack.bat
@@ -0,0 +1,33 @@
+echo off
+ REM * repack files as bin or src, using tool\7za.exe
+ REM * output in packed\x64lab.bin.0.0.21.7z
+ REM * args as <NULL>,<bin>,<src>
+	set /p vers=<version.txt
+  
+  if not exist tool\7za.exe (
+    echo - 7za.exe in the tool directory doesnt exist
+    goto end
+    )
+
+ 	if not exist packed (
+		echo - packed folder doesnt exist
+		choice  /c yn /t 3 /d n /m "- Create packed\ folder "
+		IF ERRORLEVEL 2 goto end
+		md packed
+		echo - packed directory created
+	)
+  IF "%1"=="" goto out_bin
+	IF "%1"=="src" goto out_src
+
+:out_bin
+	if exist packed\x64lab.bin.%vers%.7z del packed\x64lab.bin.%vers%.7z
+	tool\7za.exe a packed\x64lab.bin.%vers%.7z @list_bin.txt
+	IF "%1"=="bin" goto end
+
+:out_src
+	if exist packed\x64lab.src.%vers%.7z del packed\x64lab.src.%vers%.7z
+	tool\7za.exe a packed\x64lab.src.%vers%.7z @list_src.txt
+
+:end
+ set vers=
+ echo - Done !
